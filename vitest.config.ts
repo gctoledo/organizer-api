2f712a6b0ed931ee3337529d97b79d4252113841
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-30 14:47:00 -0300

    feat: add tests setup and script

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      include: ['src/**/*.ts'],
+    },
+  },
+  plugins: [tsconfigPaths()],
+})
