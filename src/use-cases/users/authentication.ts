6058b385148e20c84dc5689642cb07c94fa93660
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-30 16:42:56 -0300

    feat: add AuthenticationUseCase

---
+++ src/use-cases/users/authentication.ts
@@ -0,0 +1,42 @@
+import { InvalidCredentialsError } from '@/errors/users/invalid-credentials'
+import { ICrypto } from '@/helpers/crypto'
+import { UserRepository } from '@/repositories/interfaces/users-repository'
+import { User } from '@prisma/client'
+
+interface AuthenticationUseCaseParams {
+  email: string
+  password: string
+}
+
+interface AuthenticationUseCaseResponse {
+  user: User
+}
+
+export class AuthenticationUseCase {
+  constructor(
+    private userRepository: UserRepository,
+    private crypto: ICrypto,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticationUseCaseParams): Promise<AuthenticationUseCaseResponse> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await this.crypto.compare({
+      userPassword: user.password,
+      passwordToCompare: password,
+    })
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
