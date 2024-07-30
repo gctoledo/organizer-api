0963b69e843c58740e25fdb7598b16d00e813fe6
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-30 16:42:39 -0300

    feat: add compare method to Crypto helper

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,13 +1,25 @@
-import { hash } from 'bcryptjs'
+import { hash, compare } from 'bcryptjs'
+
+type CompareData = {
+  passwordToCompare: string
+  userPassword: string
+}
 
 export interface ICrypto {
   hash(password: string): Promise<string>
+  compare(data: CompareData): Promise<boolean>
 }
 
 export class Crypto implements ICrypto {
-  async hash(password: string): Promise<string> {
+  async hash(password: string) {
     const hashedPassword = await hash(password, 8)
 
     return hashedPassword
+  }
+
+  async compare({ userPassword, passwordToCompare }: CompareData) {
+    const isValidPassword = await compare(passwordToCompare, userPassword)
+
+    return isValidPassword
   }
 }
