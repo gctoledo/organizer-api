0f02c56231cdc1be8a29d1501c502c76f02fe9d1
Gabriel <gabrielcostatoledo@outlook.com>
2024-07-30 17:00:04 -0300

    test: ensure AuthenticationUseCase should be able to authenticate

---
+++ src/use-cases/users/authentication.test.ts
@@ -0,0 +1,39 @@
+import { Crypto } from '@/helpers/crypto'
+import { it, describe, expect } from 'vitest'
+import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+import { AuthenticationUseCase } from './authentication'
+
+describe('AuthenticationUseCase', () => {
+  const makeSut = () => {
+    const crypto = new Crypto()
+
+    const userRepository = new InMemoryUserRepository()
+
+    const sut = new AuthenticationUseCase(userRepository, crypto)
+
+    return { crypto, userRepository, sut }
+  }
+
+  it('should be able to authenticate', async () => {
+    const { sut, userRepository, crypto } = makeSut()
+
+    await userRepository.create({
+      email: 'john@doe.com',
+      first_name: 'John',
+      last_name: 'Doe',
+      password: await crypto.hash('password'),
+    })
+
+    const { user } = await sut.execute({
+      email: 'john@doe.com',
+      password: 'password',
+    })
+
+    expect(user).toEqual(
+      expect.objectContaining({
+        email: 'john@doe.com',
+      }),
+    )
+  })
+})
