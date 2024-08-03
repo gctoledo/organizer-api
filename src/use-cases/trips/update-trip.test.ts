e7f55b242249002e4abe84d0ac779b23bb426ee0
Gabriel <gabrielcostatoledo@outlook.com>
2024-08-03 01:07:56 -0300

    test: ensure UpdateTripUseCase should not be able to update trip if start date is after ends date

--- src/use-cases/trips/update-trip.test.ts
+++ src/use-cases/trips/update-trip.test.ts
@@ -3,6 +3,7 @@ import { UpdateTripUseCase } from './update-trip'
 import { InMemoryTripsRepository } from '@/repositories/in-memory/in-memory-trips-repository'
 import { InMemoryParticipantsRepository } from '@/repositories/in-memory/in-memory-participants-repository'
 import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { InvalidDateError } from '@/errors/invalid-date'
 import { NotFoundError } from '@/errors/not-found'
 import { UnauthorizedError } from '@/errors/unauthorized'
 
@@ -104,5 +105,36 @@ describe('UpdateTripUseCase', () => {
     })
 
     expect(promise).rejects.toBeInstanceOf(UnauthorizedError)
+  })
+
+  it('should not be able to update trip if start date is after ends date', async () => {
+    const user = await usersRepository.create({
+      email: 'john@doe.com',
+      first_name: 'John',
+      last_name: 'Doe',
+      password: 'password',
+    })
+
+    const { trip } = await tripsRepository.create({
+      data: {
+        destination: 'New York',
+        starts_at: new Date('2030-05-15T00:00:00.000Z'),
+        ends_at: new Date('2030-06-15T00:00:00.000Z'),
+        userId: user.id,
+      },
+      participants: [
+        { email: 'albert@doe.com', owner: false },
+        { email: 'robert@doe.com', owner: false },
+      ],
+    })
+
+    const promise = sut.execute({
+      ownerId: trip.userId,
+      tripId: trip.id,
+      destination: 'Los Angeles',
+      starts_at: new Date('2030-07-15T00:00:00.000Z'),
+    })
+
+    expect(promise).rejects.toBeInstanceOf(InvalidDateError)
   })
 })
