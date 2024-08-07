336ac66b47c784f09fd1742439a62e2e2e5d1148
Gabriel <gabrielcostatoledo@outlook.com>
2024-08-07 01:03:58 -0300

    feat: add DeleteLinkUseCase

---
+++ src/use-cases/links/delete-link.ts
@@ -0,0 +1,36 @@
+import { NotFoundError } from '@/errors/not-found'
+import { UnauthorizedError } from '@/errors/unauthorized'
+import { LinksRepository } from '@/repositories/interfaces/links-repository'
+import { TripRepository } from '@/repositories/interfaces/trips-repository'
+
+interface DeleteLinkParams {
+  userId: string
+  linkId: string
+}
+
+export class DeleteLinkUseCase {
+  constructor(
+    private linksRepository: LinksRepository,
+    private tripsRepository: TripRepository,
+  ) {}
+
+  async execute({ linkId, userId }: DeleteLinkParams) {
+    const link = await this.linksRepository.findById(linkId)
+
+    if (!link) {
+      throw new NotFoundError('Link')
+    }
+
+    const trip = await this.tripsRepository.findById(link.tripId)
+
+    if (!trip) {
+      throw new NotFoundError('Trip')
+    }
+
+    if (trip.userId !== userId) {
+      throw new UnauthorizedError()
+    }
+
+    await this.linksRepository.delete(linkId)
+  }
+}
