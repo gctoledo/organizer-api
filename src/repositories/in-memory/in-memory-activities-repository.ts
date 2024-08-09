f6eb55fae71399c07c6860a313ba938a37190f64
Gabriel <gabrielcostatoledo@outlook.com>
2024-08-09 14:10:42 -0300

    feat: add findById and update methods to InMemoryActivitiesRepository

--- src/repositories/in-memory/in-memory-activities-repository.ts
+++ src/repositories/in-memory/in-memory-activities-repository.ts
@@ -1,9 +1,30 @@
 import { randomUUID } from 'node:crypto'
 import { Prisma, Activity } from '@prisma/client'
-import { ActivitiesRepository } from '../interfaces/activities-repository'
+import {
+  ActivitiesRepository,
+  UpdateActivityParams,
+} from '../interfaces/activities-repository'
 
 export class InMemoryActivitiesRepository implements ActivitiesRepository {
   private activities: Activity[] = []
+
+  async findById(id: string) {
+    const activity = this.activities.find((activity) => activity.id === id)
+
+    if (!activity) {
+      return null
+    }
+
+    return activity
+  }
+
+  async findByTripId(tripId: string) {
+    const activities = this.activities.filter(
+      (activity) => activity.tripId === tripId,
+    )
+
+    return activities
+  }
 
   async create(data: Prisma.ActivityUncheckedCreateInput) {
     const activity = {
@@ -14,6 +35,22 @@ export class InMemoryActivitiesRepository implements ActivitiesRepository {
     }
 
     this.activities.push(activity)
+
+    return activity
+  }
+
+  async update({ data, id }: UpdateActivityParams) {
+    const index = this.activities.findIndex((activity) => activity.id === id)
+
+    let activity = this.activities[index]
+
+    if (index >= 0) {
+      activity = {
+        ...activity,
+        occurs_at: data.occurs_at,
+        title: data.title,
+      }
+    }
 
     return activity
   }
--- src/repositories/interfaces/activities-repository.ts
+++ src/repositories/interfaces/activities-repository.ts
@@ -1,5 +1,16 @@
 import { Activity, Prisma } from '@prisma/client'
 
+export interface UpdateActivityParams {
+  id: string
+  data: {
+    title: string
+    occurs_at: Date
+  }
+}
+
 export interface ActivitiesRepository {
+  findById(id: string): Promise<Activity | null>
+  findByTripId(tripId: string): Promise<Activity[]>
   create(data: Prisma.ActivityUncheckedCreateInput): Promise<Activity>
+  update(data: UpdateActivityParams): Promise<Activity>
 }
